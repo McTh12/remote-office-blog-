c538e35fdf0293dee6b857014eaeb6cc0214c71a
McTh12 <mckenziethomson@rocketmail.com>
2025-06-04 22:55:23 +0100

    Delete best-office-chairs.js

--- best-office-chairs.js
+++
@@ -1,9 +0,0 @@
-
-export default function BestOfficeChairs() {
-  return (
-    <div>
-      <h1>Top 7 Ergonomic Office Chairs for Remote Workers in 2025</h1>
-      <p>Discover chairs that boost posture and productivity. Our top pick: <a href="https://www.amazon.com/dp/B08XYZ" target="_blank" rel="noopener noreferrer">ErgoChair Pro</a>.</p>
-    </div>
-  );
-}
