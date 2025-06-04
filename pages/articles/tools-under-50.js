b4cb6a5bb69e9168ffca657445040800cb9ec797
McTh12 <mckenziethomson@rocketmail.com>
2025-06-04 23:18:46 +0100

    Create tools-under-50.js

---
+++ pages/articles/tools-under-50.js
@@ -0,0 +1,8 @@
+export default function ToolsUnder50() {
+  return (
+    <div>
+      <h1>Must-Have Productivity Tools Under $50</h1>
+      <p>Try the <a href="https://www.amazon.com/dp/B07XYZ" target="_blank" rel="noopener noreferrer">Pomodoro Timer</a> to stay focused and efficient.</p>
+    </div>
+  );
+}
