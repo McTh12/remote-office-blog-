a1d0bed43a81b53fecc12ef0ff0b93e03d492d07
McTh12 <mckenziethomson@rocketmail.com>
2025-06-04 22:59:55 +0100

    pages/articles/desk-accessories.js

---
+++ desk-accessories.js
@@ -0,0 +1,9 @@
+
+export default function DeskAccessories() {
+  return (
+    <div>
+      <h1>Best Desk Accessories for Maximum Focus</h1>
+      <p>Upgrade your desk setup. Don’t miss the <a href="https://www.amazon.com/dp/B09XYZ" target="_blank" rel="noopener noreferrer">LED Monitor Light Bar</a>.</p>
+    </div>
+  );
+}
