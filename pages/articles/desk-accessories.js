180027bb662382b18c006b4f1f3d15c28f7e7215
McTh12 <mckenziethomson@rocketmail.com>
2025-06-04 23:19:25 +0100

    Create desk-accessories.js

---
+++ pages/articles/desk-accessories.js
@@ -0,0 +1,8 @@
+export default function DeskAccessories() {
+  return (
+    <div>
+      <h1>Best Desk Accessories for Maximum Focus</h1>
+      <p>Upgrade your workspace. Try the <a href="https://www.amazon.com/dp/B09XYZ" target="_blank" rel="noopener noreferrer">LED Monitor Light Bar</a> for late-night productivity.</p>
+    </div>
+  );
+}
