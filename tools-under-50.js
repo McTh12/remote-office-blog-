bdc2eec9bd196e44cabaaff0c556eca77791c98d
McTh12 <mckenziethomson@rocketmail.com>
2025-06-04 23:11:30 +0100

    Delete tools-under-50.js

--- tools-under-50.js
+++
@@ -1,9 +0,0 @@
-
-export default function ToolsUnder50() {
-  return (
-    <div>
-      <h1>Must-Have Productivity Tools Under $50</h1>
-      <p>From smart notebooks to desk timers. Try <a href="https://www.amazon.com/dp/B07XYZ" target="_blank" rel="noopener noreferrer">Pomodoro Timer</a> to stay focused.</p>
-    </div>
-  );
-}
