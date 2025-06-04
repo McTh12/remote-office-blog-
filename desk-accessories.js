283adc3d95c0e6e3b764bb66985c0c4e60b23759
McTh12 <mckenziethomson@rocketmail.com>
2025-06-04 22:55:38 +0100

    Delete desk-accessories.js

--- desk-accessories.js
+++
@@ -1,9 +0,0 @@
-
-export default function DeskAccessories() {
-  return (
-    <div>
-      <h1>Best Desk Accessories for Maximum Focus</h1>
-      <p>Upgrade your desk setup. Don’t miss the <a href="https://www.amazon.com/dp/B09XYZ" target="_blank" rel="noopener noreferrer">LED Monitor Light Bar</a>.</p>
-    </div>
-  );
-}
